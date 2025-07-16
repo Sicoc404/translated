0b57e8b3de8ead14d039d03b05c29c12710865c7
Sicoc404 <sicoc404@gmail.com>
2025-07-16 14:30:32 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
