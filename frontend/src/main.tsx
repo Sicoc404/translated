2ba7a88e087fbe33bd0d2c5c35863d2d06b5bd71
Sicoc404 <sicoc404@gmail.com>
2025-07-17 13:46:10 +0800

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import PrymeUI from './App'
-import TestApp from './App.test'
+import PrymeUI from './App'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TestApp />
+    <PrymeUI />
   </React.StrictMode>,
 ) 
